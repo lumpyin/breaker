76dcfcb250293d478a2fdb1a6ed291020921cc40
yizhi <zyzisgood@gmail.com>
2016-08-17 01:36:00 -0500

    dd

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Vector3 padddlePos = new Vector3(0.5f,this.transform.position.y,0f);
+		
+		float mousePosInBlocks = Input.mousePosition.x/Screen.width * 16;
+		
+		padddlePos.x = Mathf.Clamp(mousePosInBlocks,0.5f,15.5f);
+		
+		this.transform.position = padddlePos;
+		print(mousePosInBlocks);
+	}
+}
